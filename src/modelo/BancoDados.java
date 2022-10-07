bfb5236fe7f9830813b175ffe3cf340b3d2273b8
Josué Melquisedeque Leite <84863364+josuemleite@users.noreply.github.com>
2022-10-06 21:16:48 -0300

    commit of sales manager

--- src/modelo/BancoDados.java
+++ src/modelo/BancoDados.java
@@ -3,6 +3,8 @@ package modelo;
 import java.util.HashMap;
 import java.util.Map;
 
+// Singleton
+
 public class BancoDados {
 	private Map<Integer, Produto> produtos = new HashMap<>();
 	private Map<String, FormaDePagamento> formasDePagamento = new HashMap<>();
@@ -15,7 +17,7 @@ public class BancoDados {
 		Produto arroz = new Produto("Arroz", 5); 
 		produtos.put(1, arroz);
 		
-		Produto feijao = new Produto("FeijÃ£o", 10);
+		Produto feijao = new Produto("Feijão", 10);
 		produtos.put(2, feijao);
 		
 		Produto milho = new Produto("Milho", 3);
@@ -24,16 +26,16 @@ public class BancoDados {
 		Produto sal = new Produto("Sal", 2);
 		produtos.put(4, sal);
 		
-		Produto acucar = new Produto("AÃ§ucar", 7); 
+		Produto acucar = new Produto("Açúcar", 7); 
 		produtos.put(5, acucar);
 		
-		Produto cafe = new Produto("CafÃ©", 12);
+		Produto cafe = new Produto("Café", 12);
 		produtos.put(6, cafe);
 		
 		Produto mel = new Produto("Mel", 20.5);
 		produtos.put(7, mel);
 		
-		Produto pao = new Produto("PÃ£o", 10);
+		Produto pao = new Produto("Pão", 10);
 		produtos.put(8, pao);
 		
 		// Simula a tabela de formas de pagamento
@@ -42,11 +44,11 @@ public class BancoDados {
 		dinheiro.setTaxaDesconto(10);
 		
 		PagamentoComDesconto debito = new PagamentoComDesconto();
-		debito.setDescricao("DÃ©bito");
+		debito.setDescricao("Débito");
 		debito.setTaxaDesconto(5);
 		
 		PagamentoComJuros credito = new PagamentoComJuros();
-		credito.setDescricao("CrÃ©dito");
+		credito.setDescricao("Crédito");
 		credito.setTaxaJuros(0);
 		
 		formasDePagamento.put("di", dinheiro);
@@ -54,7 +56,14 @@ public class BancoDados {
 		formasDePagamento.put("cc", credito);
 		
 		
-		itensEstoque.put(1, new ItemEstoque(2, arroz));
+		itensEstoque.put(1, new ItemEstoque(5, arroz));
+		itensEstoque.put(2, new ItemEstoque(3, feijao));
+		itensEstoque.put(3, new ItemEstoque(10, milho));
+		itensEstoque.put(4, new ItemEstoque(1, sal));
+		itensEstoque.put(5, new ItemEstoque(1, acucar));
+		itensEstoque.put(6, new ItemEstoque(1, cafe));
+		itensEstoque.put(7, new ItemEstoque(1, mel));
+		itensEstoque.put(8, new ItemEstoque(1, pao));
 	}
 	
 	public static BancoDados getInstancia() {
@@ -73,11 +82,25 @@ public class BancoDados {
 		return formasDePagamento.get(forma);
 	}
 	
-	public ItemEstoque selectItemEstoque(Integer codigo){
-		if (!this.itensEstoque.containsKey(codigo)) {
-			return new ItemEstoque(0, null);
+	public Integer selectQuantidadeItemEstoque(Integer codProduto){
+		ItemEstoque ie = itensEstoque.get(codProduto);
+		
+		if (ie != null) {
+			int quantidadeItem = ie.getQuantidade();
+			return quantidadeItem;
 		}
 		
-		return this.itensEstoque.get(codigo);
+		return 0;
+	}
+	
+	public void diminuiQuantidadeItemEstoque(Integer codProduto, int quantidade) {
+		ItemEstoque ie = itensEstoque.get(codProduto);
+		
+		if (ie == null) {
+			return;
+		}
+		
+		int qtdeAtual = ie.getQuantidade();
+		ie.setQuantidade(qtdeAtual - quantidade);
 	}
 }
--- src/modelo/Estoque.java
+++ src/modelo/Estoque.java
@@ -3,13 +3,21 @@ package modelo;
 import java.util.ArrayList;
 
 public class Estoque {
-	private ArrayList<ItemEstoque> itens = new ArrayList<>();
-
-	public ArrayList<ItemEstoque> getItens() {
-		return itens;
+	private BancoDados bd = BancoDados.getInstancia();
+	
+	public boolean temNoEstoque(Integer codProduto, int quantidade) {
+		return bd.selectQuantidadeItemEstoque(codProduto) >= quantidade;
 	}
-
-	public void addItens(ItemEstoque item) {
-		this.itens.add(item);
+	
+	public boolean retiraDoEstoque(Integer codProduto, int quantidade) {
+		if (temNoEstoque(codProduto, quantidade)) {
+			bd.diminuiQuantidadeItemEstoque(codProduto, quantidade);
+			return true;
+		}
+		return false;
+	}
+	
+	public Produto selectProduto(int codProduto) {
+		return bd.selectProduto(codProduto);
 	}
 }
